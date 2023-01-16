583952225975452ca6a865b5ea0d915fdf3dac5e
aaronalea <aaronalearias@gmail.com>
2023-01-16 11:26:12 -0300

    firt commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -12,31 +12,37 @@ function tienenMismaLongitud(str1, str2) {
    // Retorna True si los dos strings tienen la misma longitud.
    // De lo contrario, retorna False.
    // Tu código:
-   
+   return (str1 .lenght === str2. lenght)
 }
 
 function menosQueNoventa(num) {
    // Retorna True si el argumento "num" es menor que noventa.
    // De lo contrario, retorna False.
    // Tu código:
+   return num < 90; 
+    
 }
 
 function mayorQueCincuenta(num) {
    // Retorna True si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna False.
    // Tu código:
+   return num > 50
 }
 
 function esPar(num) {
    // Retorna True si "num" es par.
    // De lo contrario, retorna False.
    // Tu código:
+ 
+return (num % 2) === 0;
 }
 
 function esImpar(num) {
    // Retorna True si "num" es impar.
    // De lo contrario, retorna False.
    // Tu código:
+   return (num % 2) === 1
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
