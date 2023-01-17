e2042f47e7903cab7f0c14486548782323d22d8d
aaronalea <aaronalearias@gmail.com>
2023-01-17 16:13:15 -0300

    first commit

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,18 +8,30 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve False.
    // Tu código:
+   if (num > 0){
+      return "Es positivo"
+   } if (num === 0)
+   return false 
+   else{ return "Es negativo"
+   }
 }
+   
+
 
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   let nuevoString = str + "!"
+     return nuevoString;
 }
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   let nuevoString= nombre + apellido 
+   return nuevoString
 }
 
 function obtenerSaludo(nombre) {
